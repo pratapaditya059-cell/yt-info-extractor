9cd0968a5ebd89af5baf1b748e851c023795f8df
pratapaditya059-cell <pratapaditya059@gmail.com>
2026-06-12 13:02:29 +0530

    Add Video ID extraction

--- popup.js
+++ popup.js
@@ -10,7 +10,8 @@ document.addEventListener("DOMContentLoaded", async () => {
     }, (injectionResults) => {
       const data = injectionResults[0].result;
       document.getElementById("title").innerText = data.title;
-      document.getElementById("channel").innerText = data.channel;
+      document.getElementById("channel").innerText = "Channel: " + data.channel;
+      document.getElementById("videoId").innerText = "Video ID: " + data.videoId;
     });
 
   } else {
@@ -21,8 +22,12 @@ document.addEventListener("DOMContentLoaded", async () => {
 function scrapeYouTubeData() {
   const titleNode = document.querySelector('h1.ytd-watch-metadata yt-formatted-string');
   const channelNode = document.querySelector('#owner ytd-channel-name a');
+  const videoId = new URL(window.location.href)
+    .searchParams
+    .get("v");
   return {
     title: titleNode ? titleNode.innerText : document.title,
-    channel: channelNode ? channelNode.innerText : "Channel not found"
+    channel: channelNode ? channelNode.innerText : "Channel not found",
+    videoId: videoId
   };
 }
